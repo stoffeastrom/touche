8e2c7da96eff8a2d9d61018b85b3f72b5dfce643
Christoffer Åström <stoffeastrom@gmail.com>
2012-10-22 22:24:09 +0200

    added `test/simulator.js`, not done, will handle the logic to simulate a gesture

---
+++ test/simulator.js
@@ -0,0 +1,73 @@
+(function () {
+	'use strict';
+
+	var T = window.Touche || {};
+
+	T.simulate = {
+		_createTouchEvent: function (type, touchList, points) {
+			touchList = touchList || 'touches';
+
+			var event = document.createEvent('UIEvent');
+			event.initUIEvent(type, true, true);
+
+			event.view = window;
+			event.altKey = false;
+			event.ctrlKey = false;
+			event.shiftKey = false;
+			event.metaKey = false;
+			event[touchList] = [];
+			points.forEach(function (point) {
+				event[touchList].push({
+					pageX: point.x,
+					pageY: point.y
+				});
+			});
+
+			return event;
+		},
+		_createMouseEvent: function (type, touchList, points) {
+			var event, e = {
+
+				bubbles: true,
+				cancelable: (type !== "mousemove"),
+				view: window,
+				detail: 0,
+
+				screenX: points[0].x,
+				screenY: points[0].y,
+				clientX: points[0].x,
+				clientY: points[0].y,
+
+				ctrlKey: false,
+				altKey: false,
+				shiftKey: false,
+				metaKey: false,
+
+				button: 0,
+				relatedTarget: undefined
+
+			};
+
+			event = document.createEvent("MouseEvents");
+			event.initMouseEvent(type, e.bubbles, e.cancelable, e.view, e.detail, e.screenX, e.screenY, e.clientX, e.clientY, e.ctrlKey, e.altKey, e.shiftKey, e.metaKey, e.button, document.body.parentNode);
+
+			return event;
+		},
+		_createEvent: function (type, touchList, points) {
+			if(type.indexOf('touch') === 0) {
+				return this._createTouchEvent(type, touchList, points);
+			} else {
+				return this._createMouseEvent(type, touchList, points);
+			}
+		},
+		_dispatchEvent: function (elem, event) {
+			elem.dispatchEvent(event);
+			return event;
+		},
+		simulateGesture: function(target, points) {
+			/*
+			* This will handle all the logic to simulate a gesture
+			*/
+		}
+	};
+})();
