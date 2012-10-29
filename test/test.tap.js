11f8accb2be7dc7ff412cbf9febb6c4c7d92708e
Christoffer Åström <stoffeastrom@gmail.com>
2012-10-29 21:26:47 +0100

    restructured `tap.test.js`

--- test/test.tap.js
+++ test/test.tap.js
@@ -3,13 +3,17 @@ describe('Gesture', function () {
 	var body = document.body;
 
 	describe('#Tap', function () {
-		var el = document.createElement('div');
-		el.style.position = "absolute";
-		el.style.top = "0px";
-		el.style.left = "0px";
-		el.style.width = "100px";
-		el.style.height = "100px";
-		body.appendChild(el);
+		var el;
+		
+		before(function() {
+			el = document.createElement('div');
+			el.style.position = "absolute";
+			el.style.top = "0px";
+			el.style.left = "0px";
+			el.style.width = "100px";
+			el.style.height = "100px";
+			body.appendChild(el);
+		})
 
 		beforeEach(function () {
 			var context = this;
@@ -32,16 +36,16 @@ describe('Gesture', function () {
 		it('should get called when tapping in center point', function () {
 			Touche.simulate.gesture(el);
 			expect(this.called).to.be(true);
-		});
-
-		it('should not be cancelled when tapping in center point', function () {
-			Touche.simulate.gesture(el);
 			expect(this.cancelled).to.be(false);
 		});
 
 		it('should be cancelled when tapping outside element and areaThreshold', function() {
 			Touche.simulate.gesture(el, [new Touche.Point(200,200)]);
 			expect(this.cancelled).to.be(true);
+		});
+
+		after(function() {
+			body.removeChild(el);
 		});
 	});
 });
