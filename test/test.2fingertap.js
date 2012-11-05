581f98b7a536dee021a34d3bedd68978edfb5ac5
stoffeastrom <stoffeastrom@gmail.com>
2012-11-05 18:39:36 +0100

    added test for 2-finger tap

---
+++ test/test.2fingertap.js
@@ -0,0 +1,56 @@
+/*global describe, Touche, expect, it, before, beforeEach, after*/
+describe('Gesture', function () {
+	var body = document.body;
+
+	describe('#2FingerTap', function () {
+		var el;
+
+		before(function() {
+			el = document.createElement('div');
+			el.style.position = "absolute";
+			el.style.top = "0px";
+			el.style.left = "0px";
+			el.style.width = "100px";
+			el.style.height = "100px";
+			body.appendChild(el);
+		});
+
+		beforeEach(function () {
+			var context = this;
+			context.called = false;
+			context.cancelled = false;
+			context.gesture = {
+				options: {
+					touches: 2
+				},
+				end: function () {
+					context.called = true;
+				},
+				cancel: function () {
+					context.cancelled = true;
+				}
+			};
+			Touche.utils.touch = true;
+			Touche.tap(el, context.gesture);
+		});
+
+		it('should get called when tapping in center point with 2 finger', function (done) {
+		Touche.simulate.gesture(el, [new Touche.Point(50,50), new Touche.Point(50,50)], null, 'touch');
+			expect(this.called).to.be(true);
+			expect(this.cancelled).to.be(false);
+			done();
+		});
+
+		it('should not get called when tapping in center point with 3 fingers', function (done) {
+		Touche.simulate.gesture(el, [new Touche.Point(50,50), new Touche.Point(50,50), new Touche.Point(50,50)], null, 'touch');
+			expect(this.called).to.be(false);
+			expect(this.cancelled).to.be(true);
+			done();
+		});
+
+		after(function() {
+			Touche.cache.get(el).context.removeGestures('tap');
+			body.removeChild(el);
+		});
+	});
+});
