5e171ecc4e765aa54fbe975ca64525af40965681
Andrée Hansson <peolanha@gmail.com>
2013-02-03 10:17:38 +0100

    Bump version to 1.0.0

--- grunt.js
+++ grunt.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		meta: {
-			version: '0.1.0',
+			version: '1.0.0',
 			banner: '/*! Touché - v<%= meta.version %> - ' +
 				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
 				'* https://github.com/stoffeastrom/touche/\n' +
